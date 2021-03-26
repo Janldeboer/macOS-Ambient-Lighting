6a4b7fd05d72026fa4d0e312ab40551242cc5628
Jan de Boer <44832123+Janldeboer@users.noreply.github.com>
2021-03-26 15:38:56 +0100

    Introducing cropping to leave out black bars

--- Ambient Lighting/Ambient Lighting/Model/ImageSplitter/MatrixSplitter/MatrixSplitter.swift
+++ Ambient Lighting/Ambient Lighting/Model/ImageSplitter/MatrixSplitter/MatrixSplitter.swift
@@ -11,14 +11,38 @@ struct MatrixSplitter: ImageSplitter {
     
     var config: MatrixSplitterConfiguration
     
+    var widthCrop: CGFloat = 0
+    var heightCrop: CGFloat = 0
+    
     func splitImage(image: CGImage) -> [CGImage] {
         var images: [CGImage] = []
         
         let partWidth: Int = image.width / config.numberOfColumns
         let partHeight: Int = image.height / config.numberOfRows
         
+        let maxX = Int((1 - widthCrop) * CGFloat(image.width)) - partWidth
+        let minX = Int(widthCrop * CGFloat(image.width))
+        
+        let maxY = Int((1 - heightCrop) * CGFloat(image.height)) - partHeight
+        let minY = Int(heightCrop * CGFloat(image.height))
+        
         for coord in config.requiredParts {
-            let rect = CGRect(x: coord.x * partWidth, y: coord.y * partHeight, width: partWidth, height: partHeight)
+            var x = coord.x * partWidth
+            var y = coord.y * partHeight
+            
+            if x < minX {
+                x = minX
+            } else if x > maxX {
+                x = maxX
+            }
+            
+            if y < minY {
+                y = minY
+            } else if x > maxY {
+                y = maxY
+            }
+            
+            let rect = CGRect(x: x, y: y, width: partWidth, height: partHeight)
             if let cropped = image.cropping(to: rect) {
                 images.append(cropped)
             }

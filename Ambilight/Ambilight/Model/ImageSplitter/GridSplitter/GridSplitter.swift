aa6583368ac6c27e7d3fa8b8382064bea4e64c3c
Jan de Boer <44832123+Janldeboer@users.noreply.github.com>
2022-07-29 17:21:35 +0200

    Some changes...

--- Ambilight/Ambilight/Model/ImageSplitter/GridSplitter/GridSplitter.swift
+++ Ambilight/Ambilight/Model/ImageSplitter/GridSplitter/GridSplitter.swift
@@ -8,7 +8,7 @@
 import Foundation
 import SwiftUI
 
-struct GridSplitter: ImageSplitter {
+class GridSplitter: ImageSplitter, ObservableObject {
     
     var description: String = "Grid Splitter"
     
--- Ambilight/Ambilight/Model/Utility & Extensions/Numbers.swift
+++ Ambilight/Ambilight/Model/Utility & Extensions/Numbers.swift
@@ -6,9 +6,3 @@
 //
 
 import Foundation
-
-extension Comparable {
-    func clamped(to limits: ClosedRange<Self>) -> Self {
-        return min(max(self, limits.lowerBound), limits.upperBound)
-    }
-}
